b707fe70d57189ed581fcd879e418c922ef3b7a6
dlivxpr <dlivxpr@hotmail.com>
2025-05-10 09:30:51 +0800

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, rscli!");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
 }
